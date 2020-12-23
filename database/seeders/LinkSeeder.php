b42a1d7e5c3e009a14d26f78348b9002b565aadb
erika <efockereinaldo@digitalocean.com>
2020-12-23 17:12:06 +0100

    Added YAML support for including a set of basic links upon seeding

--- database/seeders/LinkSeeder.php
+++ database/seeders/LinkSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\Yaml\Yaml;
 
 class LinkSeeder extends Seeder
 {
@@ -14,9 +15,28 @@ class LinkSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('links')->insert([
-            'url' => "https://digitalocean.com/community",
-            'description' => "DigitalOcean Community"
-        ]);
+        $this->importLinks();
+    }
+
+    /**
+     * Imports Links from the default links.yml file at the root of the app.
+     * Change that file to import a set of personal basic links you want to show
+     * as soon as the application is deployed.
+     */
+    public function importLinks()
+    {
+        $links_import_path = __DIR__ . '/../../links.yml';
+
+        $yaml = new Yaml();
+        if (is_file($links_import_path)) {
+            $links = $yaml->parsefile($links_import_path);
+
+            foreach ($links as $link) {
+                DB::table('links')->insert([
+                    'url' => $link['url'],
+                    'description' => $link['description']
+                ]);
+            }
+        }
     }
 }
