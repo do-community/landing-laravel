76f0f7a1e68d23f8ca8aadb99020f58aec1f0007
Erika Heidi <erika@erikaheidi.com>
2021-08-25 12:19:13 -0300

    Eloquent Series: Tutorial 07

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,7 +36,7 @@
             @endif
         </h1>
         <p class="subtitle">
-            @foreach ($lists as $list)<a href="{{ route('link-list', $list->slug) }}" title="{{ $list->title }}" class="tag is-info is-light">{{ $list->title }}</a> @endforeach
+            @foreach ($lists as $list)<a href="{{ route('link-list', $list->slug) }}" title="{{ $list->title }}" class="tag is-info is-light">{{ $list->title }} ({{ $list->links()->count() }})</a> @endforeach
         </p>
 
         <section class="links">
